1144dd016dd0e0fd096073db2492bb5814e8ada8
Vincent Bernat <vincent@bernat.ch>
2022-11-06 01:01:33 +0100

    Add missing require

--- openbsd-knf-style.el
+++ openbsd-knf-style.el
@@ -17,7 +17,8 @@
 ;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ;;
 
-;; This function is from nicm@
+(require 'cc-mode)
+
 (defun openbsd-knf-space-indent (langelem)
   "Indents either 4 spaces or none.
 
@@ -37,25 +38,24 @@ arglist-cont-nonempty"
     (goto-char (+ (match-beginning 0) 4))
     (vector (current-column))))
 
-;; OpenBSD KNF that doug@ uses.
 (defconst openbsd-knf-style
   '(
     ;; Debugging
     ;; (c-echo-syntactic-information-p . t) ;; display C-c C-s when hit 'tab'
     ;;
     ;; General settings that should be enabled in c-mode
-    (indent-tabs-mode . t)      ;; use tabs whenever feasible
-    (fill-column . 80)          ;; Assume KNF tries to maintain 80 char lines
-    (show-trailing-whitespace . t)  ;; KNF says to not have trailing WS
-    (tab-width . 8)             ;; When displaying literal tab, show 8 spaces
+    (indent-tabs-mode . t)         ;; use tabs whenever feasible
+    (fill-column . 80)             ;; Assume KNF tries to maintain 80 char lines
+    (show-trailing-whitespace . t) ;; KNF says to not have trailing WS
+    (tab-width . 8)                ;; When displaying literal tab, show 8 spaces
     ;; c-mode
-    (c-progress-interval . 1)   ;; display progress meter during long indents
-    (c-basic-offset . 8)        ;; KNF uses 8 space tabs
-    (c-comment-only-line-offset . 0)  ;; don't indent comments extra
+    (c-progress-interval . 1) ;; display progress meter during long indents
+    (c-basic-offset . 8)      ;; KNF uses 8 space tabs
+    (c-comment-only-line-offset . 0) ;; don't indent comments extra
     (c-backspace-function . delete-backward-char)
     (c-delete-function . delete-char)
     (c-syntactic-indentation-in-macros . t) ;; indent inside macro defs
-    (c-tab-always-indent . t)  ;; indent line: Use C-q 'tab' to insert literal
+    (c-tab-always-indent . t) ;; indent line: Use C-q 'tab' to insert literal
     (c-continued-statement-offset 0)
     ;; (c-electric-flag . nil)   ;; if you don't like auto-indent
     (c-electric-continued-statement . t)
@@ -78,30 +78,30 @@ arglist-cont-nonempty"
     ;;  / = c-basic-offset * -0.5
     (c-offsets-alist . (
 			;; Literal symbols
-			(func-decl-cont . 0)        ; C++ style func mod
-			(block-open . 0)            ; '{' for block
-			(label . [1])               ; goto label in column 1
-			(comment-intro . 0)         ; C comment
-			(cpp-macro . [0])           ; #define in column 0
+			(func-decl-cont . 0)     ; C++ style func mod
+			(block-open . 0)         ; '{' for block
+			(label . [1])            ; goto label in column 1
+			(comment-intro . 0)      ; C comment
+			(cpp-macro . [0])        ; #define in column 0
 			;; Multiline macro symbols
-			(cpp-define-intro . [0])    ; first list = column 0
-			(cpp-macro-cont . +)        ; add'l lines in macro
+			(cpp-define-intro . [0]) ; first list = column 0
+			(cpp-macro-cont . +)     ; add'l lines in macro
 			;; Function symbols
-			(defun-open . 0)            ; '{' alone for func
-			(defun-close . 0)           ; '}' alone for func
-			(defun-block-intro . +)     ; first line of func
-			(topmost-intro . 0)         ; outermost part
-			(topmost-intro-cont . 0)    ; outermost part cont
-			(statement . 0)             ; func stmt (already off)
+			(defun-open . 0)         ; '{' alone for func
+			(defun-close . 0)        ; '}' alone for func
+			(defun-block-intro . +)  ; first line of func
+			(topmost-intro . 0)      ; outermost part
+			(topmost-intro-cont . 0) ; outermost part cont
+			(statement . 0)          ; func stmt (already off)
 			;; XXX statement-cont should be 4 unless
 			;; it is part of a macro, then 8.
-			(statement-cont . *)        ; continue stmt
+			(statement-cont . *) ; continue stmt
 			;; Class symbols.  XXX Should add support since there
 			;; is a little C++ in the tree (GNU)
 			;; Java
 			;; K&R
-			(knr-argdecl-intro . +)     ; rare K&R (from KNF)
-			(knr-argdecl . 0)           ; add'l indent for rest
+			(knr-argdecl-intro . +) ; rare K&R (from KNF)
+			(knr-argdecl . 0)       ; add'l indent for rest
 			;; Conditional construct symbols
 			(block-close . 0)           ; '}' for block
 			(statement-block-intro . +) ; stmt in loop/cond
@@ -111,34 +111,34 @@ arglist-cont-nonempty"
 			(do-while-closure . 0)      ; 'while' alone in 'do'
 			(else-clause . 0)           ; 'else' when not nested
 			;; Brace list symbols
-			(brace-list-close . 0)      ; enum/agg list close
-			(brace-list-intro . +)      ; 1st line of enum/agg
-			(brace-list-entry . 0)      ; add'l indent for entries
-			(brace-list-open . 0)       ; enum/agg init open
+			(brace-list-close . 0)     ; enum/agg list close
+			(brace-list-intro . +)     ; 1st line of enum/agg
+			(brace-list-entry . 0)     ; add'l indent for entries
+			(brace-list-open . 0)      ; enum/agg init open
 			;; Switch statement symbols
-			(statement-case-open . +)   ; '{' in case
-			(statement-case-intro . +)  ; 1st line in case stmt
-			(case-label . 0)            ; case label in switch
+			(statement-case-open . +)  ; '{' in case
+			(statement-case-intro . +) ; 1st line in case stmt
+			(case-label . 0)           ; case label in switch
 			;; Paren list symbols
 			;; XXX This is typically a list so need to handle it
 			;; differently from the rest.  Emacs adds the indents.
 			(arglist-intro . openbsd-knf-space-indent) ; 1st line
 			(arglist-cont . openbsd-knf-space-indent)
 			(arglist-cont-nonempty . openbsd-knf-space-indent)
-			(arglist-close . 0)         ; ')' alone
+			(arglist-close . 0)       ; ')' alone
 			;; External scope symbols
-			(extern-lang-open . [0])    ; '{' alone in 'extern C'
-			(extern-lang-close . [0])   ; '}' alone in 'extern C'
-			(inextern-lang . +)         ; lines inside 'extern C'
+			(extern-lang-open . [0])  ; '{' alone in 'extern C'
+			(extern-lang-close . [0]) ; '}' alone in 'extern C'
+			(inextern-lang . +)       ; lines inside 'extern C'
 			;; Statement block
-			(inexpr-statement . +)))    ; gcc extension stmt expr
+			(inexpr-statement . +))) ; gcc extension stmt expr
     ;; If not specified, the default is "before after".  All variables are
     ;; defined here.
     (c-hanging-braces-alist . (
 			       ;; All variables
 			       (defun-open before after)  ; function, enum
 			       (defun-close before after) ; function
-			       (class-open after) ; struct too
+			       (class-open after)         ; struct too
 			       (class-close before after)
 			       (inline-open after)
 			       (inline-close before after)
@@ -165,7 +165,7 @@ arglist-cont-nonempty"
     ;; Whether to auto-insert newline before/after colon
     (c-hanging-colons-alist . ((case-label after)
                                (label after)
-                               (access-label after)  ;; C++
+                               (access-label after) ;; C++
                                (member-init-intro before)
                                (inher-intro)))
     ;; Whether to insert newlines after ';' or ','
@@ -177,23 +177,17 @@ arglist-cont-nonempty"
 				      ;; supress newline for one liner
 				      c-semi&comma-no-newlines-for-oneline-inliners))
     ;; When autonewline mode is enabled, clean up some extra newlines
-    (c-cleanup-list . (brace-else-brace    ; } else {
-                       brace-elseif-brace  ; } else if {
-                       brace-catch-brace   ; } catch (...) {
+    (c-cleanup-list . (brace-else-brace   ; } else {
+                       brace-elseif-brace ; } else if {
+                       brace-catch-brace  ; } catch (...) {
                        ;; empty-defun-braces ; {} instead of multiple lines
-                       defun-close-semi    ; struct: no \n between '}' and ';'
-                       list-close-comma    ; remove final comma
+                       defun-close-semi ; struct: no \n between '}' and ';'
+                       list-close-comma ; remove final comma
                        scope-operator
 		       ;; space-before-funcall ; GNU standard
 		       ;; compact-empty-funcall ; another GNU standard
 		       ;; comment-close-slash ; term comment with slash
 		       ))
     ))
-
-(defun openbsd-set-knf-style ()
-  "Set OpenBSD style in a 'c-mode-common-hook'.
-Or interactively enable it in a buffer."
-  (interactive)
-  (c-add-style "OpenBSD" openbsd-knf-style t))
 
 (provide 'openbsd-knf-style)
